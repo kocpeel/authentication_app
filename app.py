f36b3d3bf24858e81c0ca64643bc73700fc09180
ShexZ <116831826+agabagbum@users.noreply.github.com>
2024-04-19 20:59:45 +0200

    x

---
+++ app.py
@@ -0,0 +1,5 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import pyotp
