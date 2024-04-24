6f126276b62e5196e73b4c6f499f55912d4b9e6c
kocpeel <134693584+kocpeel@users.noreply.github.com>
2024-04-24 18:08:19 +0200

    Login done

--- app.py
+++ app.py
@@ -43,6 +43,15 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('authenticate'))
+        flash('Invalid credentials')
+        return redirect(url_for('login'))
     return render_template('login.html')
 
 @app.route('/authenticate', methods=['GET', 'POST'])
